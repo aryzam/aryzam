c123a61f4e2b26d29dabddcecf5efd7e6599d35b
zayraz <zzambrano@gmail.com>
2026-06-09 22:11:34 +0200

    Revert "Fix: use Link component for internal navigation"
    This reverts commit 2ed9ec788ebe8d93a17a70f8b3977d402ce0d45a.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useState, useEffect, useRef } from 'react';
-import Link from 'next/link';
 
 export default function Home() {
   const [lang, setLang] = useState<'en' | 'es'>('en');
@@ -80,7 +79,7 @@ export default function Home() {
 
       {/* NAV */}
       <nav>
-        <Link href="/" className="nav-logo">Ary<em>Zam</em></Link>
+        <a href="/" className="nav-logo">Ary<em>Zam</em></a>
         <ul className="nav-links">
           <li><a href="#platform" data-en="">{t('Solutions','Soluciones')}</a></li>
           <li><a href="#compliance" data-en="">{t('Compliance','Cumplimiento')}</a></li>
@@ -338,7 +337,7 @@ export default function Home() {
 
       {/* FOOTER */}
       <footer>
-        <Link href="/" className="ft-logo">Ary<em>Zam</em></Link>
+        <a href="/" className="ft-logo">Ary<em>Zam</em></a>
         <nav className="ft-links">
           <a href="#platform">{t('Solutions','Soluciones')}</a>
           <a href="#compliance">{t('Compliance','Cumplimiento')}</a>
